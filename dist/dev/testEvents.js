e082634c4e2bc63e68402892241a54eeb2e89676
Andrei-Gabriel Dinu <andrei.dinu.dev@gmail.com>
2025-12-27 12:36:42 +0200

    chore: stop tracking dist

--- dist/dev/testEvents.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fetchEvents_1 = require("../api/fetchEvents");
-async function testEvents() {
-    const res = await (0, fetchEvents_1.fetchEvents)();
-    console.log("OK from API:", res.ok);
-    console.log("Events:", res.data);
-}
-testEvents().catch(console.error);
